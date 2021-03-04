ed47f116b45e5b318dc4ff21399afae2d6f3906c
vladukhaDog <vladic.p@gmail.com>
2021-03-04 16:05:33 +0300

    Initial Commit

---
+++ barHelper/barHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  barHelperApp.swift
+//  barHelper
+//
+//  Created by vladukha on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct barHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
