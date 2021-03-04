7d32e6da92f35e3b2c5a8c5eee43d3096ac862af
vladukhaDog <vladic.p@gmail.com>
2021-03-04 17:56:10 +0300

    some normal list of drinks

--- barHelper/Views/DrinkRow.swift
+++ barHelper/Views/DrinkRow.swift
@@ -7,11 +7,33 @@
 
 import SwiftUI
 
+struct ComponentList: View {
+	var components : [Component]
+	var body: some View {
+		VStack{
+			ForEach(components, id: \.self) { component in
+				HStack{
+					Text(String(component.liqID ?? 0) + " - " + String(component.amount ?? 0) + "ml")
+					Spacer()
+				}
+			}
+		}
+	}
+}
+
 struct DrinkRow: View {
 	let drink : Drink
 	
     var body: some View {
-		Text(drink.name ?? "NoName")
+		VStack{
+			HStack{
+				Text(drink.name ?? "NoName")
+					.font(.title)
+				Spacer()
+			}
+			ComponentList(components: drink.recipe!)
+		}
+		
     }
 }
 
