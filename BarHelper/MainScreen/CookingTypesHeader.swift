e8990d0b189907e9d64792b4e6b1bd691f5b5c2f
vladukha <vladic.p@gmail.com>
2024-09-23 18:11:09 +0300

    Made a header with mock data for cocktail cooking types

---
+++ BarHelper/MainScreen/CookingTypesHeader.swift
@@ -0,0 +1,64 @@
+//
+//  CookingTypesHeader.swift
+//  BarHelper
+//
+//  Created by Владислав Пермяков on 23.09.2024.
+//
+
+import SwiftUI
+
+struct CookingTypesHeader: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            title
+            existingList
+        }
+        .padding(8)
+        .background(Color.blue.opacity(0.15))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    private var title: some View {
+        Text("Cooking methods")
+            .font(.title)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var existingList: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                addButton
+                ForEach(0..<2) { ai in
+                    Color.red
+                        .aspectRatio(1.0, contentMode: .fit)
+                }
+            }
+            .padding(8)
+        }
+        .background(Color.primary.colorInvert())
+        .frame(height: 100)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+    
+    private var addButton: some View {
+        Button{
+            
+        } label: {
+            Color.clear.opacity(0.15)
+                .aspectRatio(1.0, contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+                .overlay {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(Color.primary.opacity(0.2))
+                        .padding(20)
+                }
+        }
+    }
+}
+
+#Preview {
+    CookingTypesHeader()
+        .padding()
+}
