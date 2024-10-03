ad655047b1df242de258a9f3a37ab5d0a5accbcf
vladukha <vladic.p@gmail.com>
2024-10-03 17:37:31 +0300

    добавил картинки на экран методов готовки

--- BarHelper/CookingMethodsView/CookingMethodsView.swift
+++ BarHelper/CookingMethodsView/CookingMethodsView.swift
@@ -15,51 +15,110 @@ struct CookingMethodsView<ViewModel>: View where ViewModel: CookingMethodsViewMo
     
     @StateObject private var vm: ViewModel
     @State private var showAdd = false
-    var body: some View {
-        old
-            .sheet(item: $vm.methodToView) { item in
-                EditCookingMethodView(item)
-                    .presentationDetents([.medium, .large])
-            }
-    }
 
-    var old: some View {
-        VStack(alignment: .trailing) {
-            ScrollView {
-                LazyVStack {
-                    ForEach(vm.methods, id: \.id) { method in
-                        Button {
-                            vm.methodToView = method
-                        } label: {
-                            Text(method.name ?? "")
-                                .cyberpunkFont(.smallTitle)
-                                .foregroundColor(.white)
-                        }
-                        .padding(5)
-                    }
-                }
-                .padding(.horizontal, 5)
-            }
-            .frame(maxWidth: .infinity)
-            .background(Color.black)
-            .padding(5)
-            .depthBorder()
-            .padding(.horizontal, 5)
-            
-            Button("Add") {
-                showAdd.toggle()
-            }
-            .cyberpunkStyle(.green)
-            .frame(width: 150)
-            .sheet(isPresented: $showAdd) {
-                addCookingMethod
-                .presentationDetents([.medium, .large])
-            }
-            .padding(.horizontal)
-            
+    var body: some View {
+        VStack(alignment: .center) {
+            methodImages
+            list
+                .border(.blue)
+            addButon
+                .alignmentGuide(HorizontalAlignment.center, computeValue: {d in d[.leading]})
         }
+        .padding(.vertical, 5)
+        .padding(5)
+        .depthBorderUp()
+        .padding(10)
         .backgroundWithoutSafeSpace(.darkPurple)
         .navigationTitle("Cooking types")
+        .sheet(item: $vm.methodToView) { item in
+            EditCookingMethodView(item)
+                .presentationDetents([.medium, .large])
+        }
+    }
+    
+    private var list: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(vm.methods, id: \.id) { method in
+                    Button {
+                        vm.methodToView = method
+                    } label: {
+                        Text(method.name ?? "")
+                            .cyberpunkFont(.smallTitle)
+                            .foregroundColor(.white)
+                    }
+                    .padding(5)
+                }
+            }
+            .padding(.horizontal, 5)
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.black)
+        .padding(5)
+        .depthBorder()
+        .padding(.horizontal, 5)
+    }
+    
+    private var methodImages: some View {
+        HStack {
+            Spacer()
+            ZStack(alignment: .bottom) {
+                ZStack(alignment: .bottom) {
+                        Image("icesprite")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.leading, 10)
+                            .padding(.bottom, 5)
+                        Image("icesprite")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.trailing, 10)
+                    }
+                .padding(5)
+                Image("spoon")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.leading, 10)
+                    .padding(.bottom, 8)
+                Image("glass")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .frame(width: 50)
+            Spacer()
+            shaker
+            Spacer()
+        }
+        .frame(height: 100)
+        .background(Color.black)
+        .padding(5)
+        .depthBorder()
+        .padding(.horizontal, 5)
+    }
+    
+    private var shaker: some View {
+        VStack(spacing: 0){
+            Image("shaker_top")
+                .resizable()
+                .scaledToFit()
+            Image("shaker_bottom")
+                .resizable()
+                .scaledToFit()
+        }
+        .aspectRatio(0.5, contentMode: .fit)
+    }
+    
+    private var addButon: some View {
+        Button("New") {
+            showAdd.toggle()
+        }
+        .cyberpunkStyle(.green)
+        .frame(width: 150)
+        .sheet(isPresented: $showAdd) {
+            addCookingMethod
+            .presentationDetents([.medium, .large])
+        }
+        .padding(.horizontal)
     }
     
     private var addCookingMethod: some View{
